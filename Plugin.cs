6cd9c00cb00f125149f446a5fee786f43e7a906d
qstage <166386817+qstage@users.noreply.github.com>
2024-10-24 22:50:42 +0300

    plugin version without depending gamedata

--- Plugin.cs
+++ Plugin.cs
@@ -1,74 +1,54 @@
-﻿using System.Runtime.InteropServices;
+﻿using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
-using CounterStrikeSharp.API.Modules.Memory;
-using CounterStrikeSharp.API.Modules.Memory.DynamicFunctions;
+using CounterStrikeSharp.API.Modules.Cvars;
 
 namespace FixRandomSpawn;
 
-public unsafe class Plugin : BasePlugin
+public class Plugin : BasePlugin
 {
     public override string ModuleName => "FixRandomSpawn";
     public override string ModuleVersion => "1.0.0";
     public override string ModuleAuthor => "xstage";
 
-    private readonly List<byte> oldBytes = [];
-    private byte* ptr;
-
-    private static MemoryFunctionWithReturn<nint, nint>? MaintainDMSpawnPopulation;
-    
-    [DllImport("kernel32.dll")]
-    private static extern bool VirtualProtect(nint address, uint dwSize, int newProtect, int* oldProtect);
+    private CBaseEntity? lastSpawn;
+    private ConVar? mp_randomspawn;
 
     public override void Load(bool hotReload)
     {
-        string signature = GameData.GetSignature("FindAsmInstruction");
-        ptr = (byte*)NativeAPI.FindSignature(Addresses.ServerPath, signature);
-        int length = signature.Count(c => c == ' ') + 1;
-        bool isLinux = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+        mp_randomspawn = ConVar.Find("mp_randomspawn");
 
-        if (!isLinux)
-        {
-            int *oldProtect = stackalloc int[1];
-            VirtualProtect((nint)ptr, (uint)length, 0x40, oldProtect);
-        }
-        else
-        {
-            MaintainDMSpawnPopulation = new(GameData.GetSignature("MaintainDMSpawnPopulation"));
-            MaintainDMSpawnPopulation.Hook(Hook_MaintainDMSpawnPopulation, HookMode.Post);
-        }
-
-        for (int i = 0; i < length; i++)
-        {
-            oldBytes.Add(ptr[i]);
-
-            if (isLinux)
-            {
-                int nextByte = i + 1;
-
-                if (nextByte < length && ptr[i] == 0x0f && ptr[nextByte] == 0x85)
-                {
-                    oldBytes.Add(ptr[nextByte]);
-
-                    ptr[i] = 0x90;
-                    ptr[nextByte] = 0xe9;
-
-                    break;
-                }
-            }
-
-            ptr[i] = 0x90;
-        }
+        RegisterEventHandler<EventPlayerSpawn>(OnPlayerSpawn);
     }
 
-    private HookResult Hook_MaintainDMSpawnPopulation(DynamicHook hook) => HookResult.Continue;
-
-    public override void Unload(bool hotReload)
+    private CBaseEntity GetRandomSpawn(List<CBaseEntity> spawnList)
     {
-        for (int i = 0; i < oldBytes.Count; i++)
-        {
-            ptr[i] = oldBytes[i];
-        }
+        CBaseEntity? spawn = null;
 
-        MaintainDMSpawnPopulation?.Unhook(Hook_MaintainDMSpawnPopulation, HookMode.Post);
+        while ((spawn = spawnList.ElementAtOrDefault(new Random().Next(0, spawnList.Count - 1))) == null || spawn == lastSpawn)
+        {}
+        lastSpawn = spawn;
+
+        return spawn;
+    }
+
+    private HookResult OnPlayerSpawn(EventPlayerSpawn @event, GameEventInfo info)
+    {
+        int randomType = (mp_randomspawn?.GetPrimitiveValue<int>()).GetValueOrDefault();
+        var spawnList = Utilities.FindAllEntitiesByDesignerName<CBaseEntity>("info_deathmatch_spawn").ToList();
+
+        if (spawnList.Count == 0 || randomType == 0)
+            return HookResult.Continue;
+
+        var player = @event.Userid;
+        var pawn = player?.PlayerPawn.Value;
+        var spawn = GetRandomSpawn(spawnList);
+
+        if (player == null || pawn == null || spawn.AbsOrigin == null)
+            return HookResult.Continue;
+
+        if (randomType == 1 || randomType == player.TeamNum)
+            pawn.Teleport(spawn.AbsOrigin, spawn.AbsRotation);
+
+        return HookResult.Continue;
     }
 }
